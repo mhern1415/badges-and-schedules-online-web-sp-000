9c8252da61feb4dbafc65e7c7b89218b1b94d4bb
Mark Hernandez <m.hern1415@gmail.com>
2019-05-03 17:50:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,11 +19,11 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |value|
-    puts value
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
   end
-  assign_rooms(attendees).each do |value|
-    puts value
+  assign_rooms(attendees).each do |badge|
+    puts badge
   end
 end
 
