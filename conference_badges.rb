6627b7af464c41b1cf36f5e46eb117eca378bec4
Mark Hernandez <m.hern1415@gmail.com>
2019-05-03 17:19:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,7 @@ end
 def batch_badge_creator(attendees)
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 attendees.each do |name|
-  
+  return "Hello, my name is #{name}."
 end
+
 
