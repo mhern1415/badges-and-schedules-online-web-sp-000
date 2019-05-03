805a59d5295944d868fec26a186bea14464b579c
Mark Hernandez <m.hern1415@gmail.com>
2019-05-03 17:46:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,4 +18,11 @@ def assign_rooms(attendees)
   end
 end
 
+def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
 
